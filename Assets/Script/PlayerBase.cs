e5270e3634cf6b1b2a37270bf1c8a388458f5371
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2022-08-31 17:08:00 +0900

    Playerとステージ制作中

--- Assets/Script/PlayerBase.cs
+++ Assets/Script/PlayerBase.cs
@@ -6,30 +6,64 @@ using UnityEngine;
 public class PlayerBase : MonoBehaviour
 {
     [SerializeField]
+    [Header("プレイヤーのタイプ")]
+    PlayerType _playerType;
+
+    [SerializeField]
     [Header("スピード")]
-    float _speed = 5;
+    float _speed = 15;
 
     Rigidbody _rb;
 
-    void Awake()
+    string _hName;
+    string _vName;
+    
+    const string HORIZONTAL = "Horizontal";
+    const string VERTICAL = "Vertical";
+    const string HORIZONTAL2 = "Horizontal2";
+    const string VERTICAL2 = "Vertical2";
+
+    protected virtual void Awake()
     {
         _rb = GetComponent<Rigidbody>();
+        switch(_playerType)
+        {
+            case PlayerType.Player1:
+                _hName = HORIZONTAL;
+                _vName = VERTICAL;
+                break;
+            case PlayerType.Player2:
+                _hName = HORIZONTAL2;
+                _vName = VERTICAL2;
+                break;
+            default:
+                Debug.Log("プレイヤーのタイプを設定してください");
+                break;
+        }
     }
 
-    void Update()
+    protected virtual void Update()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        OnMove();
+
+        Move();
     }
 
-    void OnMove()
+    protected virtual void Move()
     {
-        //_rb.AddForce(new Vector3(_m.x, 0f, _m.y) * _speed);
-        float h = Input.GetAxis("Mouse X");
-        float v = Input.GetAxis("Mouse Y");
-        _rb.velocity = new Vector3(h,0f,v) * _speed;
+        float h = Input.GetAxisRaw(_hName);
+        float v = Input.GetAxisRaw(_vName);
+        _rb.velocity = new Vector3(h, 0f, v).normalized * _speed;
     }
-    
+
+    //void MoveForMouse()
+    //{
+    //    //_rb.AddForce(new Vector3(_m.x, 0f, _m.y) * _speed);
+    //    float h = Input.GetAxis("Mouse X");
+    //    float v = Input.GetAxis("Mouse Y");
+    //    _rb.velocity = new Vector3(h,0f,v) * _speed;
+    //}
+
     //public void Move(InputAction.CallbackContext context)
     //{
     //    _m = context.ReadValue<Vector2>();
