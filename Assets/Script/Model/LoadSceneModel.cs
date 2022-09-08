4bbd770abde495702aef7de599d116ad8e3dc641
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2022-09-08 12:20:19 +0900

    名前を変更

--- Assets/Script/Model/LoadSceneModel.cs
+++ Assets/Script/Model/LoadSceneModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneLoader : SingletonMonoBehaviour<SceneLoader>
+public class LoadSceneModel : MonoBehaviour
 {
     public void LoadScene(string name)
     {
